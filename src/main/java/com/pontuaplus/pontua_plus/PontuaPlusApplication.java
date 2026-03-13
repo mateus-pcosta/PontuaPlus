24bb1493326f57ed2561fda32fe1e255af3a7c99
Mateus Pessoa Costa <mateuspessoacosta0@gmail.com>
2026-03-13 11:42:50 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/pontuaplus/pontua_plus/PontuaPlusApplication.java
@@ -0,0 +1,13 @@
+package com.pontuaplus.pontua_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontuaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontuaPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pontuaplus/pontua_plus/PontuaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontuaplus.pontua_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontuaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
